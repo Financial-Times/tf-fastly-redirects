98147972dc7e272c5280f45417860983e6b03dc0
Deyan Valkanov <deyan.valkanov@ft.com>
2020-07-06 13:15:28 +0300

    Add support fot terraform v0.12

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 A Terraform module for simplifying redirects across Fastly services.
 Internally this generates VCL snippets for inclusion in a Fastly service, serving a 302 response code.
 
-Currently only supports Terraform v0.11
-Requires Fastly v0.8 or higher.
+This branch supports Terraform v0.12
+Requires Fastly v0.12 or higher.
 
 ## Path Format
 Paths are case-insensitive, and are defined with a leading slash and without a trailing slash, i.e. `/an-example-link`.
@@ -13,6 +13,7 @@ Paths are case-insensitive, and are defined with a leading slash and without a t
 1. Define a new variable for storing redirects.
 ```terraform
 variable "path-redirects" {
+    type    = list(map(string))
     default = [
         {"/this-is-a-test" = "https://www.example.com/"}
     ]
@@ -22,13 +23,13 @@ variable "path-redirects" {
 2. Import this module
 ```terraform
 module "fastly-redirect" {
-  source = "github.com/financial-times/terraform-module-fastly-redirect"
+  source = "github.com/financial-times/tf-fastly-redirects?ref=redirects-v0.12"
 
   # Error code, configurable to avoid clashing with other VCL.
   error-code = "917"
 
   # A list containing redirects.
-  path-redirect-block = ["${var.path-redirects}"]
+  path-redirect-block = var.path-redirects
 }
 ```
 
@@ -40,14 +41,14 @@ resource "fastly_service_v1" "fastly-redirects" {
   snippet {
     name = "tf-fastly-redirects-recv"
     type = "recv"
-    content = "${module.fastly-redirects.tf-fastly-redirects-recv}"
+    content = module.fastly-redirect.tf-fastly-redirects-recv
   }
 
   # Serve generated redirects.
   snippet {
     name = "tf-fastly-redirects-error"
     type = "error"
-    content = "${module.fastly-redirects.tf-fastly-redirects-error}"
+    content = module.fastly-redirect.tf-fastly-redirects-error
   }
 }
 ```
--- main.tf
+++ main.tf
@@ -1,57 +1,66 @@
-terraform {
-  required_version = "~> 0.11"
-}
 
 data "template_file" "tf-fastly-redirects-error" {
-  template = "${file("${path.module}/vcl/snippet/tf-fastly-redirects-error.vcl.tpl")}"
+  template = file(
+    "${path.module}/vcl/snippet/tf-fastly-redirects-error.vcl.tpl",
+  )
 
   vars = {
-    error-code = "${var.error-code}"
+    error-code = var.error-code
   }
 }
 
 data "template_file" "tf-fastly-redirects-recv" {
-  template = "${file("${path.module}/vcl/snippet/tf-fastly-redirects-recv.vcl.tpl")}"
+  template = file(
+    "${path.module}/vcl/snippet/tf-fastly-redirects-recv.vcl.tpl",
+  )
 
   vars = {
-    rendered-redirects = "${indent(2, join("\n", data.template_file.tf-path-fastly-redirect-partial.*.rendered))}"
+    rendered-redirects = indent(
+      2,
+      join(
+        "\n",
+        data.template_file.tf-path-fastly-redirect-partial.*.rendered,
+      ),
+    )
   }
 }
 
 data "template_file" "tf-path-fastly-redirect-partial" {
-  template = "${file("${path.module}/vcl/partial/tf-path-fastly-redirect-partial.vcl.tpl")}"
-  count    = "${length(var.path-redirect-block)}"
+  template = file(
+    "${path.module}/vcl/partial/tf-path-fastly-redirect-partial.vcl.tpl",
+  )
+  count = length(var.path-redirect-block)
 
   vars = {
-    destination = "${element(values(var.path-redirect-block[count.index]), 0)}"
-    source      = "${element(keys(var.path-redirect-block[count.index]), 0)}"
-    error-code  = "${var.error-code}"
+    destination = element(values(var.path-redirect-block[count.index]), 0)
+    source      = element(keys(var.path-redirect-block[count.index]), 0)
+    error-code  = var.error-code
   }
 }
 
 variable "error-code" {
-  type    = "string"
+  type    = string
   default = "942"
 }
 
 variable "path-redirect-block" {
-  type = "list"
+  type = list(map(string))
 
   default = [
     {
       "/test/" = "https://example.com/"
-    }
+    },
   ]
 }
 
-
 # Outputs
 output "tf-fastly-redirects-recv" {
-  value = "${data.template_file.tf-fastly-redirects-recv.rendered}"
+  value       = data.template_file.tf-fastly-redirects-recv.rendered
   description = "Fastly snippet to catch paths must be redirected"
 }
 
 output "tf-fastly-redirects-error" {
-  value = "${data.template_file.tf-fastly-redirects-error.rendered}"
+  value       = data.template_file.tf-fastly-redirects-error.rendered
   description = "Fastly snippet to serve redirects"
 }
+
